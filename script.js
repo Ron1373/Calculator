7cab665a7b7d1fab794b024551dfc0634377582b
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-04-26 20:04:52 -0700

    Adds the operate function which takes the operator and two numbers and calls the corresponding function

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+let firstNum, secondNum;
+
+let operator;
+
+function operate(firstNum, operator, secondNum){
+    switch (operator){
+        case "+":
+            return add(firstNum, secondNum);
+            break;
+
+        case "-":
+            return subtract(firstNum, secondNum);
+            break;
+        case "*":
+            return multiply(firstNum, secondNum);
+            break;
+        case "/":
+            return divide(firstNum, secondNum);
+            break;
+    }
+}
